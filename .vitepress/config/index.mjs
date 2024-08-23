e0389aa0e7dab0ad33743d2ff398aec66be4a50b
otabekoff <sadiridinovotabek@gmail.com>
2024-08-24 02:35:54 +0500

    Fix for GitHub Pages

--- .vitepress/config/index.mjs
+++ .vitepress/config/index.mjs
@@ -1,22 +1,23 @@
-import { defineConfig } from "vitepress";
-import { genFeed } from "../genFeed.js";
-import head from "./head";
-import themeConfig from "./themeConfig";
-import nav from "./nav";
-import search from "./search";
-import sidebar from "./sidebar";
-import socialLinks from "./socialLinks";
-import footer from "./footer";
-import editLink from "./editLink";
-import lastUpdated from "./lastUpdated";
-import carbonAds from "./carbonAds";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vitepress'
+import { genFeed } from '../genFeed.js'
+import head from './head'
+import themeConfig from './themeConfig'
+import nav from './nav'
+import search from './search'
+import sidebar from './sidebar'
+import socialLinks from './socialLinks'
+import footer from './footer'
+import editLink from './editLink'
+import lastUpdated from './lastUpdated'
+import carbonAds from './carbonAds'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Dev docs",
-  lang: "uz",
-  base: "/",
+  title: 'Dev docs',
+  lang: 'uz',
+  // base: '/', // for localhost
+  base: '/dev-docs/', // for pages
   head,
   description:
     "Dasturlash tillari va texnologiyalarining O'zbek tilidagi interaktiv qo'llanmalari uchun platforma.",
@@ -31,9 +32,9 @@ export default defineConfig({
     footer,
     // carbonAds,
     blog: {
-      title: "My Blog",
-      description: "Some articles for sample Blog",
-    },
+      title: 'My Blog',
+      description: 'Some articles for sample Blog'
+    }
   },
 
   editLink,
@@ -48,5 +49,5 @@ export default defineConfig({
   // build: {
   //   timeout: 30000, // Increase build timeout (e.g., 30 seconds)
   // },
-  buildEnd: genFeed,
-});
+  buildEnd: genFeed
+})
